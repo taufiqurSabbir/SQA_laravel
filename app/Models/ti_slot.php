dd3ba8bae9ddb179e2d5061c7309a970b75a45ef
taufiqurSabbir <taufiqur.sabbir11@gmail.com>
2024-05-08 08:15:17 +0600

    protected $guarded

---
+++ app/Models/ti_slot.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ti_slot extends Model
+{
+    use HasFactory;
+
+    protected $guarded=[];
+}
